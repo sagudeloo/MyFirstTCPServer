f1cbb1adef82406dd2095234689c846b802e64df
sagudeloo <sagudeloo@eafit.edu.co>
2020-11-09 12:06:34 -0500

    Implementacionde la comunicacion entr el cliente y el servidor a excepcion de las funciones de upload y download

--- client.py
+++ client.py
@@ -1,4 +1,7 @@
 import socket
+import logging
+import threading
+import sys
 
 class Client:
 
@@ -10,27 +13,110 @@ class Client:
     
     # This function start connecte the client with the server an continue de execution
     def run(self):
-        self.sock.connect((self.host, self.port))
+        try:
+            self.sock.connect((self.host, self.port))
+        except:
+            print("Unable to establish a connection with the server.")
+            sys.exit(1)
         print(f"Connected to {self.host}:{self.port}")
-        self.sendCmd("lsfl pruebita")
-        x = self.readResp()
-        print(x)
+        try:
+            while True:
+                cmd = self.readCmd()
+                commandStatus = self.interpretCmd(cmd)
+                if commandStatus:
+                    resp = self.readResp().decode(self.encoding)
+                    print(resp[1:])
+        except KeyboardInterrupt:
+            self.sock.close()
+            print("\nThe Client has been closed.")
+            sys.exit(1)
+
+    def readCmd(self):
+        rawCmd = input(">>> ")
+        rawCmd = rawCmd.strip()
+        return rawCmd
+
+    def interpretCmd(self, command):
+        splitedCmd = command.split(" ")
+        cmd = splitedCmd[0]
+        args = splitedCmd[1:]
+        if (cmd == "mkbk"):
+            if (len(args) >= 1):
+                self.sendCmd(cmd+" "+args[0])
+                return True
+            else:
+                print("Missing arguments")
+                return False
+        elif (cmd == "rmbk"):
+            if (len(args) >= 1):
+                self.sendCmd(cmd+" "+args[0])
+                return True
+            else:
+                print("Missing arguments")
+                return False
+        elif (cmd == "rmfl"):
+            if (len(args) >= 2):
+                self.sendCmd(cmd+" "+args[0]+" "+args[1])
+                return True
+            else:
+                print("Missing arguments")
+                return False
+        elif (cmd == "lsbk"):
+            self.sendCmd(cmd)
+        elif (cmd == "lsfl"):
+            if (len(args) >= 2):
+                self.sendCmd(cmd+" "+args[0]+" "+args[1])
+                return True
+            else:
+                print("Missing arguments")
+                return False
+        elif (cmd == "upfl"):
+            if (len(args) >= 2):
+                x = threading.Thread(target=self.uploadFile, args=(args[0],args[1]))
+                x.start()
+                x.join()
+                return True
+            else:
+                print("Missing arguments")
+                return False
+        elif (cmd == "dwfl"):
+            if (len(args) >= 2):
+                threading.Thread(target=self.downloadFile, args=(args[0],args[1]))
+                x.start()
+                x.join()
+                return True
+            else:
+                print("Missing arguments")
+                return False
+        elif (cmd == "help"):
+            return False
+        else:
+            return False
     
     """ Commands
         mkbk: create a new bucket
         rmbk: delete a bucket
+        rmfl: delete file
         lsbk: list the buckets
         lsfl: list file in a bucket
         upfl: upload file
-        dwfl: download file
-        rmfl: delete file """
+        dwfl: download file """
+
     # This function send the client commands to the server
     def sendCmd(self, command):
         self.sock.sendall(command.encode(self.encoding))
 
     def readResp(self):
         data = self.sock.recv(1024)
-        return data.decode(self.encoding)
+        return data
+
+    def uploadFile(self, bucketName, fileName):
+        pass
+
+    def downloadFile(self, bucketName, fileName):
+        self.sendCmd("dwfl"+" "+bucketName+" "+fileName)
+        
+
 
 c = Client()
 c.run()
--- server.py
+++ server.py
@@ -6,36 +6,42 @@ import threading
 class Server:
 
     def __init__(self, port=4321, host="localhost", path=".", encoding="UTF-8"):
-        self.sock = socket.socket()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
         self.port = port
         self.path = path
-        self.connections = dict()
+        self.connections = list()
         self.sock.bind((self.host, self.port))
         self.encoding = encoding
     
-    def run(self): 
-        self.sock.listen(10)
-        print("Server started...")
-        i = 0
-        while True:
-            conn, addr = self.sock.accept()
-            print(f"Accepted connection from {addr}.")
-            if (len(self.connections) < 10):
-                x = threading.Thread(target=self.runClient, args=(i, conn, addr,))
+    def run(self):
+        try:
+            self.sock.listen(10)
+            print("Server started...")
+            i = 0
+            while True:
+                conn, addr = self.sock.accept()
+                print(f"Accepted connection from {addr}.")
+                # connType = self.read(conn).decode(self.encoding)
+                x = threading.Thread(target=self.runClient, args=(conn, addr,))
                 x.start()
-                self.connections[i] = x
-                i += 1
+                self.connections.append(x)
+            for x in connections:
+                x.join()
+        except KeyboardInterrupt:
+            self.sock.close()
 
     # This funtion run a thread with the client connection
-    def runClient(self, index, conn, addr): 
+    def runClient(self, conn, addr): 
         connState = True
         print("Connection thread created.")
         while connState:
             cmd = self.read(conn).decode(self.encoding)
             if cmd:
-                cmdResoult = self.interpretCmd(cmd)
-                self.send(conn, bytes(cmdResoult, self.encoding))
+                print(f"Command: {cmd}, from {addr}")
+                cmdResponse = self.interpretCmd(cmd)
+                print(f"Response: {cmdResponse}, to {addr}")
+                self.send(conn, bytes(cmdResponse, self.encoding))
 
     # This funtion wait for a command from client connection
     def read(self, conn):
@@ -67,13 +73,13 @@ class Server:
         elif (cmdKey == "lsfl"):
             return self.listFiles(args[1])
         elif (cmdKey == "upfl"):
-            return self.uploadFile(args[1], args[2])
+            return self.recv(args[1], args[2])
         elif (cmdKey == "dwfl"):
-            return self.downloadFile(args[1], args[2])
+            return self.sendFile(args[1], args[2])
         elif (cmdKey == "rmfl"):
             return self.removeFile(args[1], args[2])
         else:
-            return "0 Command not found."
+            return "Command not found."
 
     def makeBucket(self, bucketName):
         if (not self.bucketExists(bucketName)):
@@ -95,21 +101,37 @@ class Server:
             for entry in it:
                 if (not entry.name.startswith('.')) and (not entry.is_file()):
                     buckets += entry.name + "\n"
-        return buckets
+        return "0 " + buckets
 
     def listFiles(self, bucketName):
         files = ""
-        with os.scandir(self.path+"/"+bucketName) as it:
-            for entry in it:
-                if (not entry.name.startswith('.')) and (entry.is_file()):
-                    files += entry.name + "\n"
-        return files
+        if (self.bucketExists(bucketName)):
+            with os.scandir(self.path+"/"+bucketName) as it:
+                for entry in it:
+                    if (not entry.name.startswith('.')) and (entry.is_file()):
+                        files += entry.name + "\n"
+            return "0 " + files
+        else:
+            return "1 Bucket does not exist."
     
-    def uploadFile(self, bucketName, fileName):
+    def recvFile(self):
         pass
     
-    def downloadFile(self, bucketName, fileName):
-        pass
+    def sendFile(self, conn):
+
+        if (bucketExists(bucketName)):
+            if(fileExists(bucketName, fileName)):
+                send(conn, b"0 Okay")
+                with open(self.path+"/"+bucketName+"/"+fileName, "rb") as file:
+                    data = file.read(1024)
+                while (data):
+                    send(conn, data)
+                    data = file.read(1024)
+            else:
+                return "1 File does not exist."
+        else:
+            return "1 Bucket does not exist."
+        return "0 Transmission finished."
     
     def removeFile(self, bucketName, fileName):
         if (os.path.exists(self.path+"/"+bucketName+"/"+fileName)):
@@ -118,12 +140,11 @@ class Server:
         else:
             return "1 File does not exist."
 
-    def bucketExists(self, name):
-        with os.scandir(".") as it:
-            for entry in it:
-                if (not entry.name.startswith('.')) and (not entry.is_file()) and (entry.name == name):
-                    return True
-        return False
+    def bucketExists(self, bucketName):
+        return os.path.isdir(self.path+"/"+bucketName)
+    
+    def fileExists(self, bucketName, fileName):
+        return os.path.isfile(self.path+"/"+bucketName+"/"+fileName)
 
 s = Server()
 s.run()
